9dae5d6d242462d12e823e3a182bf2398221d30e
Trần Thanh Hải <96519556+haiThanhTran@users.noreply.github.com>
2025-01-17 12:51:55 +0700

    Prepare for deployment

---
+++ build/precache-manifest.90f262aae79aa22c4332122af86d3131.js
@@ -0,0 +1,234 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c9b6a7bc0f63b39c783a3c1f1394e3e",
+    "url": "/Manage_Employee/index.html"
+  },
+  {
+    "revision": "5f25ae4012391b8fdf9b",
+    "url": "/Manage_Employee/static/css/10.01bfbfa9.chunk.css"
+  },
+  {
+    "revision": "d340a7f6856e7900bfd9",
+    "url": "/Manage_Employee/static/css/11.8fd06f8e.chunk.css"
+  },
+  {
+    "revision": "8ea4aea810be263a5538",
+    "url": "/Manage_Employee/static/css/12.8fd06f8e.chunk.css"
+  },
+  {
+    "revision": "f4c4139da7dbcf07f64a",
+    "url": "/Manage_Employee/static/css/13.8fd06f8e.chunk.css"
+  },
+  {
+    "revision": "b05d9eaa60784ca12f77",
+    "url": "/Manage_Employee/static/css/14.8fd06f8e.chunk.css"
+  },
+  {
+    "revision": "bc786f3f4636d8858dc1",
+    "url": "/Manage_Employee/static/css/15.8fd06f8e.chunk.css"
+  },
+  {
+    "revision": "fdd38f0000c5aab6a1de",
+    "url": "/Manage_Employee/static/css/16.ca1e8814.chunk.css"
+  },
+  {
+    "revision": "a71a278e36485ccac588",
+    "url": "/Manage_Employee/static/css/17.41621948.chunk.css"
+  },
+  {
+    "revision": "54542a4591319e5717dd",
+    "url": "/Manage_Employee/static/css/18.a4c84b4d.chunk.css"
+  },
+  {
+    "revision": "17c235c72fd8f26cd06b",
+    "url": "/Manage_Employee/static/css/19.ca1e8814.chunk.css"
+  },
+  {
+    "revision": "8b4a32e991a9e21f66cb",
+    "url": "/Manage_Employee/static/css/26.9ab26ee2.chunk.css"
+  },
+  {
+    "revision": "8d4335e252e0d6757273",
+    "url": "/Manage_Employee/static/css/29.fc9e39b3.chunk.css"
+  },
+  {
+    "revision": "0171502e4a9ceae8e47d",
+    "url": "/Manage_Employee/static/css/main.57f1442f.chunk.css"
+  },
+  {
+    "revision": "10f68440c17a08e19272",
+    "url": "/Manage_Employee/static/js/0.b3d65a7c.chunk.js"
+  },
+  {
+    "revision": "13c537e8e0e34ca784c6",
+    "url": "/Manage_Employee/static/js/1.263ee279.chunk.js"
+  },
+  {
+    "revision": "5f25ae4012391b8fdf9b",
+    "url": "/Manage_Employee/static/js/10.5338d860.chunk.js"
+  },
+  {
+    "revision": "d340a7f6856e7900bfd9",
+    "url": "/Manage_Employee/static/js/11.ad505ac7.chunk.js"
+  },
+  {
+    "revision": "8ea4aea810be263a5538",
+    "url": "/Manage_Employee/static/js/12.4015a427.chunk.js"
+  },
+  {
+    "revision": "f4c4139da7dbcf07f64a",
+    "url": "/Manage_Employee/static/js/13.4e23c5f0.chunk.js"
+  },
+  {
+    "revision": "b05d9eaa60784ca12f77",
+    "url": "/Manage_Employee/static/js/14.16447c1d.chunk.js"
+  },
+  {
+    "revision": "bc786f3f4636d8858dc1",
+    "url": "/Manage_Employee/static/js/15.d8a6e05e.chunk.js"
+  },
+  {
+    "revision": "fdd38f0000c5aab6a1de",
+    "url": "/Manage_Employee/static/js/16.74b843ad.chunk.js"
+  },
+  {
+    "revision": "a71a278e36485ccac588",
+    "url": "/Manage_Employee/static/js/17.705deca1.chunk.js"
+  },
+  {
+    "revision": "54542a4591319e5717dd",
+    "url": "/Manage_Employee/static/js/18.b164ece5.chunk.js"
+  },
+  {
+    "revision": "17c235c72fd8f26cd06b",
+    "url": "/Manage_Employee/static/js/19.15a7fcea.chunk.js"
+  },
+  {
+    "revision": "4c149e150a1b9fcddc11",
+    "url": "/Manage_Employee/static/js/2.4fbd44e6.chunk.js"
+  },
+  {
+    "revision": "86f9507d18a0d91cdcd8",
+    "url": "/Manage_Employee/static/js/20.5a9a374d.chunk.js"
+  },
+  {
+    "revision": "bc96ccb5ab6a32d30dd5",
+    "url": "/Manage_Employee/static/js/21.858917bf.chunk.js"
+  },
+  {
+    "revision": "c558ab5a8ea36d5701c0",
+    "url": "/Manage_Employee/static/js/22.3d660865.chunk.js"
+  },
+  {
+    "revision": "74c8ec1685c58667b2be",
+    "url": "/Manage_Employee/static/js/23.889853af.chunk.js"
+  },
+  {
+    "revision": "12008d29dca1f9b02595",
+    "url": "/Manage_Employee/static/js/24.fbe7a510.chunk.js"
+  },
+  {
+    "revision": "40a2ecdcdeebde59ab00",
+    "url": "/Manage_Employee/static/js/25.1b015fcb.chunk.js"
+  },
+  {
+    "revision": "8b4a32e991a9e21f66cb",
+    "url": "/Manage_Employee/static/js/26.794c14cc.chunk.js"
+  },
+  {
+    "revision": "27c8be707e46e4a25fc1",
+    "url": "/Manage_Employee/static/js/27.d369e802.chunk.js"
+  },
+  {
+    "revision": "7994f6781d4a379a2f68",
+    "url": "/Manage_Employee/static/js/28.e81cc589.chunk.js"
+  },
+  {
+    "revision": "8d4335e252e0d6757273",
+    "url": "/Manage_Employee/static/js/29.afdb56b7.chunk.js"
+  },
+  {
+    "revision": "7d9ca8b0fe631a636c86",
+    "url": "/Manage_Employee/static/js/3.17226757.chunk.js"
+  },
+  {
+    "revision": "525586848de3715c7fef",
+    "url": "/Manage_Employee/static/js/30.25a0dfcb.chunk.js"
+  },
+  {
+    "revision": "a73a0e7837406b587af8",
+    "url": "/Manage_Employee/static/js/31.e2b19f8b.chunk.js"
+  },
+  {
+    "revision": "7d110ba71ef1569429e6",
+    "url": "/Manage_Employee/static/js/32.55e096e1.chunk.js"
+  },
+  {
+    "revision": "59e90833d05fe6d00e4d",
+    "url": "/Manage_Employee/static/js/33.88bd8c62.chunk.js"
+  },
+  {
+    "revision": "833e45a3920401f01539",
+    "url": "/Manage_Employee/static/js/34.37fe0678.chunk.js"
+  },
+  {
+    "revision": "8b06bfcacea44cdcc7cc",
+    "url": "/Manage_Employee/static/js/35.7602ebbc.chunk.js"
+  },
+  {
+    "revision": "1a5277fccab6ff1fd06e",
+    "url": "/Manage_Employee/static/js/36.871edb77.chunk.js"
+  },
+  {
+    "revision": "257df4bd0704ca8c203b",
+    "url": "/Manage_Employee/static/js/37.0a27b4d8.chunk.js"
+  },
+  {
+    "revision": "05234fe3e32638cf8e2e",
+    "url": "/Manage_Employee/static/js/38.40b34921.chunk.js"
+  },
+  {
+    "revision": "c9317a7e1aa63ea9ee51",
+    "url": "/Manage_Employee/static/js/39.145e3d4a.chunk.js"
+  },
+  {
+    "revision": "d9d51d1d8d1cc0434520",
+    "url": "/Manage_Employee/static/js/4.af9da3b0.chunk.js"
+  },
+  {
+    "revision": "e681c8655a55f40ba809",
+    "url": "/Manage_Employee/static/js/40.b97c095e.chunk.js"
+  },
+  {
+    "revision": "c6e0e561b84a6ac3843f",
+    "url": "/Manage_Employee/static/js/41.5573c953.chunk.js"
+  },
+  {
+    "revision": "f02ebc41d57d8802aee8",
+    "url": "/Manage_Employee/static/js/42.e3baafb5.chunk.js"
+  },
+  {
+    "revision": "f59fdc9368af5a23f3ee",
+    "url": "/Manage_Employee/static/js/43.39bd72a3.chunk.js"
+  },
+  {
+    "revision": "bf6418777bba2a2bf2b8",
+    "url": "/Manage_Employee/static/js/5.b391e391.chunk.js"
+  },
+  {
+    "revision": "727134eaaa01ba8f916d",
+    "url": "/Manage_Employee/static/js/6.b9777a3d.chunk.js"
+  },
+  {
+    "revision": "56d74fc8eedfc72ae355",
+    "url": "/Manage_Employee/static/js/7.ed91dee0.chunk.js"
+  },
+  {
+    "revision": "0171502e4a9ceae8e47d",
+    "url": "/Manage_Employee/static/js/main.4a4110b3.chunk.js"
+  },
+  {
+    "revision": "998dace6283d37192a32",
+    "url": "/Manage_Employee/static/js/runtime~main.4a25a5e3.js"
+  }
+]);
